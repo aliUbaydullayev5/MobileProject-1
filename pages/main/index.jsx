bc0a9a07888eacb4789fc3ce4bec66902603a4fb
alicoder5 <mukhammadali.ubaydullayev.05@inbox.ru>
2022-11-08 00:29:36 +0500

    test

--- pages/main/index.jsx
+++ pages/main/index.jsx
@@ -1,8 +1,14 @@
+import styled from 'styled-components'
+
+const Block = styled.div`
+  border: 1px solid red;
+`
+
 const Main = () => {
     return(
-        <div>
+        <Block>
             Main
-        </div>
+        </Block>
     )
 }
 
